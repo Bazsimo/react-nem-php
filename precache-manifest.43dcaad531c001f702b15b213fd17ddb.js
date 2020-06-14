c331f52069cddaa7416eba93306a5a8100527239
Bazsimo <balala.bazsi@gmail.com>
2020-06-14 21:09:20 +0200

    deploying

---
+++ precache-manifest.43dcaad531c001f702b15b213fd17ddb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d57c7e3c9623f3fbdac3acd2d1e10f",
+    "url": "/react-nem-php/index.html"
+  },
+  {
+    "revision": "393512d802130a5a0c6f",
+    "url": "/react-nem-php/static/css/2.2b4849b6.chunk.css"
+  },
+  {
+    "revision": "1b4e270a55427db07718",
+    "url": "/react-nem-php/static/css/main.a1a63c61.chunk.css"
+  },
+  {
+    "revision": "393512d802130a5a0c6f",
+    "url": "/react-nem-php/static/js/2.43aea200.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-nem-php/static/js/2.43aea200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4e270a55427db07718",
+    "url": "/react-nem-php/static/js/main.219ec985.chunk.js"
+  },
+  {
+    "revision": "85931ef5e76e1eb209e8",
+    "url": "/react-nem-php/static/js/runtime-main.c3ae2fb7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-nem-php/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-nem-php/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-nem-php/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-nem-php/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-nem-php/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
